527d6843e8cb87fe600463db2f134651713bf3f8
Andrej Leban <andrejleban@gmail.com>
2018-12-09 07:13:46 +0100

    day9

---
+++ 9/9.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 20:15:20 2018
+
+@author: Andrej Leban
+"""
+
+import itertools as it
+import functools as ft
+import collections as coll
+
+import sortedcontainers as sc
+from blist import blist
+
+import re
+
+#re.search('@ (\d+),(\d+)', item).groups()))
+
+def parseInput(inp):
+    data = []
+    with open(inp, 'r') as f:
+        for line in f:
+            data.append(line)
+
+    return data
+
+
+def cwise(curr, n, lst):
+    if curr is None:
+        curr = 0
+    if not len(lst):
+        return 1
+    return (curr + n) % len(lst)
+
+
+def ccwise(curr, n, lst):
+    if curr is None:
+        curr = 0
+    if not len(lst):
+        return 1
+    return (curr - n) % len(lst)
+
+if __name__ == "__main__":
+
+    maxM = 7162800
+    nplayers = 448
+    data = [i for i in range(1, maxM+1)]
+#    data = data[:-1] + [7162800]
+
+    cycle = blist([0])
+    current = 0
+
+    kept = [set() for i in range(nplayers)]
+    elves = it.cycle(range(nplayers))
+    elf = elves.__next__()
+
+    for m in data:
+#        print(cycle, cycle[current], current)
+        #tie break
+        if not m % 23:
+
+            irem = ccwise(current, 7, cycle)
+            rem = cycle.pop(irem)
+
+            kept[elf].add(m)
+            kept[elf].add(rem)
+
+            current = irem
+            elf = elves.__next__()
+            continue
+
+        i = cwise(current, 2, cycle)
+
+        if len(cycle) == 1 and i == 0:
+            cycle = cycle + [m]
+            i = 1
+        else:
+            cycle.insert(i, m)
+
+        current = i
+        elf = elves.__next__()
+
+#iI = np.where(cycle == 0)
+#iI = cycle.index(0)
+#cycle2 = cycle[iI:] + cycle[:iI]
+
+score = list(map(sum, kept))
+print(max(score))
+
+#for k in kept:
+#    if maxM in k:
+#        k.remove(maxM)
+#        k.add(maxM * 100)
+#
+#score = list(map(sum, kept))
+#print(max(score))
+
